84ee5a044715f64737af5745ee31d80148163a55
fcampoa <felixcampoa@gmail.com>
2025-05-25 23:07:52 -0700

    migrations

--- migrations/AlmaDeMalta.Migrations/Program.cs
+++ migrations/AlmaDeMalta.Migrations/Program.cs
@@ -6,25 +6,25 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
-var config = new ConfigurationBuilder()
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json")
-    .AddEnvironmentVariables()
-    .Build();
+//var config = new ConfigurationBuilder()
+//    .SetBasePath(Directory.GetCurrentDirectory())
+//    .AddJsonFile("appsettings.json")
+//    .AddEnvironmentVariables()
+//    .Build();
 
-var servicesCollection = new ServiceCollection();
+//var servicesCollection = new ServiceCollection();
 
-servicesCollection.UseMongoConfig(config);
-var provider = servicesCollection.BuildServiceProvider();
-var migrations = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.IsAssignableTo(typeof(IMigration)) && !t.IsAbstract);
-foreach (var migration in migrations)
-{
-    var migrationInstance = ActivatorUtilities.CreateInstance(provider, migration);
-    if (migrationInstance is IMigration migrationToExecute)
-    {
-        await migrationToExecute.Execute();
-    }
-}
+//servicesCollection.UseMongoConfig(config);
+//var provider = servicesCollection.BuildServiceProvider();
+//var migrations = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.IsAssignableTo(typeof(IMigration)) && !t.IsAbstract);
+//foreach (var migration in migrations)
+//{
+//    var migrationInstance = ActivatorUtilities.CreateInstance(provider, migration);
+//    if (migrationInstance is IMigration migrationToExecute)
+//    {
+//        await migrationToExecute.Execute();
+//    }
+//}
 Console.WriteLine("Migrations executed successfully.");
 
 
