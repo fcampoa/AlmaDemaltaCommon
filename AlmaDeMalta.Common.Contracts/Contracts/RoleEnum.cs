5cc3c17b8fc0183cb9e1b318940c0b7eb562f1b7
fcampoa <felixcampoa@gmail.com>
2025-07-02 18:17:00 -0700

    auth

---
+++ AlmaDeMalta.Common.Contracts/Contracts/RoleEnum.cs
@@ -0,0 +1,8 @@
+﻿
+namespace AlmaDeMalta.Common.Contracts.Contracts;
+public enum RoleEnum
+{
+    Admin = 0,
+    User = 1,
+    Guest = 2
+}
---
+++ AlmaDeMalta.Common.Contracts/Contracts/User.cs
@@ -0,0 +1,15 @@
+﻿
+
+using AlmaDeMalta.Common.Contracts.Attributes;
+
+namespace AlmaDeMalta.Common.Contracts.Contracts;
+[CollectionName("Users")]
+public class User : BaseEntity
+{
+    public override IList<string> ItemType => [nameof(User)];
+    public string Name { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string? AuthProviderId { get; set; } = string.Empty;
+    public string Phone { get; set; } = null!;
+    public RoleEnum Role { get; set; } = RoleEnum.User;
+}
